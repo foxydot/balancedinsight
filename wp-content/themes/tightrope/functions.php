766c175b17857c9ad96ddb27dca686aaa94eac68
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2013-12-11 19:47:31 -0500

    tweak it to "my way"

--- wp-content/themes/tightrope/functions.php
+++ wp-content/themes/tightrope/functions.php
@@ -1,97 +1,31 @@
 <?php
-function regMyWidgets(){
-    register_sidebar( array(
-        'name' => __( 'Home Widgets Left','theme707' ),
-        'before_widget' => '<div id="%1$s" class="widget_style %2$s">',
-	'after_widget' => '</div>',
-	'before_title' => '<h2 id="homewidgleft">',
-	'after_title' => '</h2>',
-    ));
+/*
+ * Pull in some stuff from other files
+*/
+if(!function_exists('requireDir')){
+    function requireDir($dir){
+        $dh = @opendir($dir);
 
-    register_sidebar( array(
-        'name' => __( 'Home Widgets Middle','theme707' ),
-        'before_widget'=> '<div id="%1$s" class="widget_style %2$s">',
-	'after_widget' => '</div>',
-	'before_title' => '<h2 id="homewidgmiddle">',
-	'after_title' => '</h2>',
-    ));
-
-    register_sidebar( array(
-        'name' => __( 'Home Widgets Right','theme707' ),
-        'before_widget' => '<div id="%1$s" class="widget_style %2$s">',
-	'after_widget' => '</div>',
-	'before_title' => '<h2 id="homewidgright">',
-	'after_title' => '</h2>',
-    ));
-
-    register_sidebar( array(
-	'name' => __( 'Sidebar','theme707' ),
-	'before_widget' => '<div id="%1$s" class="widget_style %2$s">',
-	'after_widget' => '</div>',
-	'before_title' => '<h2>',
-	'after_title' => '</h2>',
-    ));
-}
-
-add_action( 'widgets_init', 'regMyWidgets' );
-
-function tm_date($d='', $before='', $after='', $echo = true) {
-	global $id, $post, $day, $previousday, $newday;
-	$the_date = '';
-	$the_date .= $before;
-	if ( $d=='' )
-		$the_date .= mysql2date(get_option('date_format'), $post->post_date);
-	else
-		$the_date .= mysql2date($d, $post->post_date);
-	$the_date .= $after;
-	$previousday = $day;
-	$the_date = apply_filters('the_date', $the_date, $d, $before, $after);
-	if ( $echo )
-		echo $the_date;
-	else
-		return $the_date;
-}
-
-function regMyMenus(){
-  register_nav_menus( array(
-    'top-nav' => __( 'Top-Level Navigation' ),
-    'mini-nav' => __( 'Small Top-Level Navigation' ),
-    'footer-nav' => __( 'Footer Navigation' ),
-
-  ));
-}
-
-add_action( 'after_setup_theme', 'regMyMenus' );
-
-function topnav_fallback() {
-?>
-<!-- Fallback menu -->
-<ul id="top-nav" role="navigation">
-<li><a href="index.php?page_id=44">Explore Consensus</a></li>
-<li><a href="index.php?page_id=70">Services</a></li>
-<li><a href="index.php?page_id=78">Partners</a></li>
-<li><a href="index.php/?page_id=16">Company</a></li>
-</ul> <!-- #top-nav -->
-<?php
-}
-?>
-<?php
-function mininav_fallback() {
-?>
-<!-- Fallback menu -->
-<ul id="mini-nav" role="navigation">
-<li><a href="wp-login.php">Customer Login</a></li>
-<li><a href="index.php?page_id=66">Support</a></li>
-<li><a href="http://www.balancedinsight.com/blog/">Blog</a></li>
-<li><a href="index.php?page_id=252">Contact Us</a></li>
-</ul> <!-- #mini-nav -->
-<?php
-}
-
-add_action('wp_enqueue_scripts','msdlab_add_scripts');
-function msdlab_add_scripts() {
-    global $is_IE;
-    if(!is_admin()){
-        wp_enqueue_script('msd-jquery',get_stylesheet_directory_uri().'/theme-jquery.js',array('jquery'));
+        if (!$dh) {
+            throw new Exception("Cannot open directory $dir");
+        } else {
+            while($file = readdir($dh)){
+                $files[] = $file;
+            }
+            closedir($dh);
+            sort($files); //ensure alpha order
+            foreach($files AS $file){
+                if ($file != '.' && $file != '..') {
+                    $requiredFile = $dir . DIRECTORY_SEPARATOR . $file;
+                    if ('.php' === substr($file, strlen($file) - 4)) {
+                        require_once $requiredFile;
+                    } elseif (is_dir($requiredFile)) {
+                        requireDir($requiredFile);
+                    }
+                }
+            }
+        }
+        unset($dh, $dir, $file, $requiredFile);
     }
 }
+requireDir(get_stylesheet_directory() . '/lib/inc');
---
+++ wp-content/themes/tightrope/lib/inc/register-scripts.php
@@ -0,0 +1,33 @@
+<?php
+/*
+* Add styles and scripts
+*/
+add_action('wp_print_styles', 'msd_add_styles');
+
+function msd_add_styles() {
+    global $is_IE;
+    if(!is_admin()){
+        wp_enqueue_style('msd-style',get_stylesheet_directory_uri().'/lib/css/style.css');
+        if($is_IE){
+            wp_enqueue_script('ie-style',get_stylesheet_directory_uri().'/lib/css/ie.css');
+        }
+        if(is_front_page()){
+            wp_enqueue_style('msd-homepage-style',get_stylesheet_directory_uri().'/lib/css/homepage.css');
+        }
+    }
+}
+add_action('wp_print_scripts', 'msd_add_scripts');
+
+function msd_add_scripts() {
+    global $is_IE;
+    if(!is_admin()){
+        wp_enqueue_script('msd-jquery',get_stylesheet_directory_uri().'/lib/js/theme-jquery.js',array('jquery'));
+        if($is_IE){
+            wp_enqueue_script('columnizr',get_stylesheet_directory_uri().'/lib/js/jquery.columnizer.js');
+            wp_enqueue_script('ie-fixes',get_stylesheet_directory_uri().'/lib/js/ie-jquery.js');
+        }
+        if(is_front_page()){
+            wp_enqueue_script('msd-homepage-jquery',get_stylesheet_directory_uri().'/lib/js/homepage-jquery.js',array('jquery','jquery-scrollto'));
+        }
+    }
+}
